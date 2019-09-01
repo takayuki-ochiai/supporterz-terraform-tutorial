88534dedaaecd74c441b49c5936969d345dd4d8d
Takayuki Ochiai <go.ahead.oti@gmail.com>
2019-09-01 14:51:20 +0900

    ELB作ってMultiAZな状態にする

--- main.tf
+++ main.tf
@@ -27,6 +27,20 @@ resource "aws_subnet" "public_0" {
   }
 }
 
+resource "aws_subnet" "public_1" {
+  cidr_block = "10.0.2.0/24"
+  vpc_id     = aws_vpc.terraform_example_vpc.id
+
+  // このサブネットで起動したインスタンスにパブリックIPアドレスを自動的に割り当てる
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
+
+  tags = {
+    Name      = "public-1"
+    ManagedBy = "Terraform"
+  }
+}
+
 resource "aws_internet_gateway" "terraform_example_igw" {
   vpc_id = aws_vpc.terraform_example_vpc.id
 
@@ -36,7 +50,7 @@ resource "aws_internet_gateway" "terraform_example_igw" {
   }
 }
 
-resource "aws_route_table" "terraform_public" {
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.terraform_example_vpc.id
 
   tags = {
@@ -46,65 +60,109 @@ resource "aws_route_table" "terraform_public" {
 }
 
 // 先ほど作成したルートテーブルに対してレコードを挿入する
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.terraform_public.id
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
   gateway_id             = aws_internet_gateway.terraform_example_igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
 // サブネットにルートテーブルを関連づけする
 resource "aws_route_table_association" "public_0" {
-  route_table_id = aws_route_table.terraform_public.id
+  route_table_id = aws_route_table.public_route_table.id
   subnet_id      = aws_subnet.public_0.id
 }
 
-
-module "ec2_security_group" {
-  source      = "./modules/security_group"
-  name        = "ec2_security_group"
-  vpc_id      = aws_vpc.terraform_example_vpc.id
-  port        = 80
-  cidr_blocks = ["0.0.0.0/0"]
+resource "aws_route_table_association" "public_1" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_1.id
 }
 
-resource "aws_eip" "ec2_instance" {
-  vpc = true
-  instance = aws_instance.example_0.id
+// セキュリティグループの設定
+resource "aws_security_group" "terraform_example" {
+  name   = "terraform-example-security-group"
+  vpc_id = aws_vpc.terraform_example_vpc.id
 
   tags = {
-    Name      = "ec2_instance"
+    Name      = "example"
     ManagedBy = "Terraform"
   }
 }
 
-data "aws_ami" "recent_amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
+resource "aws_security_group_rule" "ingress_rule" {
+  // インバウンドルールの追加
+  type = "ingress"
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
-  }
+  // ポート範囲を指定
+  from_port = 80
+  to_port   = 80
 
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
+  // プロトコルの指定
+  protocol          = "tcp"
+  security_group_id = aws_security_group.terraform_example.id
+
+  // 許可するトラフィックのIP範囲をCIDRで指定
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_instance" "example_0" {
-  ami           = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public_0.id
+// 全てのアウトバウンドトラフィックを許可する
+resource "aws_security_group_rule" "egress_rule" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.terraform_example.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-  // セキュリティグループはsecurity_groupsではなくvpc_security_group_idsで設定すること
-  // さもないとセキュリティグループの設定を追加した時にインスタンス削除→再構築されてしまう!
-  vpc_security_group_ids = [module.ec2_security_group.security_group_id]
+module "http_server_0" {
+  source            = "./modules/http_server"
+  server_name       = "http-server-0"
+  subnet_id         = aws_subnet.public_0.id
+  security_group_id = aws_security_group.terraform_example.id
+}
 
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+module "http_server_1" {
+  source            = "./modules/http_server"
+  server_name       = "http-server-1"
+  subnet_id         = aws_subnet.public_1.id
+  security_group_id = aws_security_group.terraform_example.id
+}
+
+resource "aws_elb" "terrafrom_example_elb" {
+  name = "terraform-example-elb"
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:80"
+    interval = 30
+  }
+
+  subnets = [
+    aws_subnet.public_0.id,
+    aws_subnet.public_1.id,
+  ]
+
+  security_groups = [
+    aws_security_group.terraform_example.id,
+  ]
+
+  instances = [
+    module.http_server_0.ec2_instance_id,
+    module.http_server_1.ec2_instance_id,
+  ]
+
+  tags = {
+    Name      = "foobar-terraform-elb"
+    ManagedBy = "Terraform"
+  }
 }
 
---
+++ modules/http_server/main.tf
@@ -0,0 +1,53 @@
+variable "server_name" {}
+variable "subnet_id" {}
+variable "security_group_id" {}
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+
+resource "aws_eip" "ec2_instance" {
+  vpc = true
+  instance = aws_instance.example.id
+
+  tags = {
+    Name      = "${var.server_name}-eip"
+    ManagedBy = "Terraform"
+  }
+}
+
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  // セキュリティグループはsecurity_groupsではなくvpc_security_group_idsで設定すること
+  // さもないとセキュリティグループの設定を追加した時にインスタンス削除→再構築されてしまう
+  vpc_security_group_ids = [var.security_group_id]
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
+
+  tags = {
+    Name      = "${var.server_name}"
+    ManagedBy = "Terraform"
+  }
+}
