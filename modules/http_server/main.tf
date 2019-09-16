158112596ce7626c1d80b5f8a9057081c8a7f065
Takayuki Ochiai <go.ahead.oti@gmail.com>
2019-09-16 15:39:31 +0900

    コメントの追加

--- modules/http_server/main.tf
+++ modules/http_server/main.tf
@@ -6,14 +6,20 @@ variable "instance_type" {
 }
 
 data "aws_ami" "recent_amazon_linux_2" {
+  // 条件に当てはまる複数の結果が返ってきたとき、最新のAMIを返す
   most_recent = true
+  // AMIの所有者情報で絞り込む。今回はAWS公式AMIを使うためamazonを指定
   owners      = ["amazon"]
 
+  // AMIの検索条件
+  // aws-cliのdescribe-imagesと記法は同じ
+  // AMIの名前を条件に検索
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
   }
 
+  // AMIのstateを2つ目の条件に追加
   filter {
     name   = "state"
     values = ["available"]
